e55565ca6eb4e9d2fd77b90a7f32deb86e2681ef
Dan <dcolish@gmail.com>
2010-08-09 21:22:37 -0700

    crawl and gen graph, then make some pictures

--- scraper.py
+++ scraper.py
@@ -6,21 +6,41 @@ from pprint import PrettyPrinter
 latest_commits = []
 pp = PrettyPrinter()
 base_url = "http://github.com/api/v2/json"
-search_term = "python"
-s = base_url + "/repos/search/%s?fork=false"
-resp = urlopen(s % search_term)
-resp_json = JSONDecoder().decode(resp.read())
-repoids = [(x.get('id'), x.get('username'), x.get('name'))
-           for x in resp_json['repositories']]
+
+
+def data_parse(resp, key):
+    try:
+        resp_json = JSONDecoder().decode(resp.read())
+        return resp_json[key]
+    except:
+        return []
+
+
+def get_top_repos(search_term):
+    s = base_url + "/repos/search/%s?fork=false"
+    resp = urlopen(s % search_term)
+    resp_json = JSONDecoder().decode(resp.read())
+    return [(x.get('id'), x.get('username'), x.get('name'))
+            for x in resp_json['repositories']]
 
 
 def get_latest_commit(user, repo):
     query = base_url + "/commits/list/%s/%s/master" % (user, repo)
     resp = urlopen(query)
-    resp_json = JSONDecoder().decode(resp.read())
-    return resp_json['commits'][0]
+    key = 'commits'
+    return data_parse(resp, key)
 
-latest_commits = [get_latest_commit(user, repo)
-                  for repoid, user, repo in repoids]
 
-pp.pprint([x['message'] for x in latest_commits])
+def get_user_following(user):
+    query = base_url + "/user/show/%s/following" % user
+    resp = urlopen(query)
+    key = 'users'
+    return data_parse(resp, key)
+
+
+def get_user_followers(user):
+    query = base_url + "/user/show/%s/followers" % user
+    resp = urlopen(query)
+    key = 'users'
+    return data_parse(resp, key)
+
